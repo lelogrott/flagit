5709d5f621ba0545fdc39aaee62e8c6aa280fddc
Aurelio Grott <grott.aurelio@gmail.com>
2019-03-26 01:55:40 -0700

    quick test patchup

--- test/test_flagit.rb
+++ test/test_flagit.rb
@@ -9,7 +9,7 @@ class FlagitTest < Minitest::Test
       @flagit_mock.expect(:hi, 'Text - <commit_message> <commit_url>', ['Text'])
     end
     def test_hi
-      Flagit.stub :hi, @flagit_mock do
+      Flagit.stub :run, @flagit_mock do
         assert_equal 'Text - <commit_message> <commit_url>', @flagit_mock.hi('Text')
       end
       @flagit_mock.verify
