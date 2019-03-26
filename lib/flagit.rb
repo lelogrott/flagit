84fb6af316de453de318104a0019b2cd8eedc9fc
Aurelio Grott <grott.aurelio@gmail.com>
2019-03-26 02:54:25 -0700

    properly storing credentials

--- lib/flagit.rb
+++ lib/flagit.rb
@@ -2,7 +2,7 @@ class Flagit
   attr_accessor :configuration
 
   def self.run(message, path = '.')
-    Flagit.load
+    load
     lookout = Flagit::Lookout.new(path)
     redactor = Flagit::Redactor.new(lookout)
     Flagit::Postman.tweet(redactor.write_content(message))
@@ -27,19 +27,24 @@ class Flagit
   end
 
   def self.save!
-    File.open('.flagit-1.0.0.config.json',"w") do |f|
-      f.write(Flagit.configuration.config_to_json)
+    File.open("#{root}/.config.json","w") do |f|
+      f.write(configuration.config_to_json)
     end
   end
 
   def self.load
-    loaded = JSON.parse(File.read('.flagit-1.0.0.config.json'))
-    Flagit.configure do |config|
+    return unless File.exist?("#{root}/.config.json")
+    loaded = JSON.parse(File.read("#{root}/.config.json"))
+    configure do |config|
       config.consumer_key = loaded['consumer_key']
       config.consumer_secret = loaded['consumer_secret']
       config.access_token = loaded['access_token']
       config.access_token_secret = loaded['access_token_secret']
     end
+  end
+
+  def self.root
+    File.dirname __dir__
   end
 end
 
